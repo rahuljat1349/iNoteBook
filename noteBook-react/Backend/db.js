a630ddea8e95c8640a75174114d044bcec7f2c9f
Rahul Dudi <rahuldudi1349@gmail.com>
2024-01-07 19:29:48 +0530

    starting

--- noteBook-react/Backend/db.js
+++ noteBook-react/Backend/db.js
@@ -2,13 +2,9 @@ const mongoose = require("mongoose");
 
 const url = "mongodb://localhost:27017/iNoteBook";
 
-
-
-
-let dbConnect = async() => {
-   await mongoose.connect(url); 
-   console.log('Connected to Mongo successfully');   
+let dbConnect = async () => {
+  await mongoose.connect(url);
+  console.log("Connected to Mongo successfully");
 };
- 
 
 module.exports = dbConnect;
--- noteBook-react/Backend/index.js
+++ noteBook-react/Backend/index.js
@@ -1,15 +1,14 @@
-const dbConnect = require('./db');
-const express = require('express');
+const dbConnect = require("./db");
+const express = require("express");
 dbConnect();
 
 const app = express();
 const port = 8000;
 
-app.get('/',(req,res)=>{
-res.send("hello")
-})
+// Avilable Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use('/api/notes',require('./routes/notes'));
 
-
-app.listen(port,()=>{
-    console.log(`srever is listening on post ${port}`);
+app.listen(port, () => {
+  console.log(`srever is listening on post ${port}`);
 });
