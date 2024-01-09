93142ecef5e3366438f11d207ca2bac6212ae74a
Rahul Dudi <rahuldudi1349@gmail.com>
2024-01-09 20:34:15 +0530

    created token and encryption

--- noteBook-react/Backend/routes/auth.js
+++ noteBook-react/Backend/routes/auth.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'rahul@sign';
 
 // create a user using post
 router.post(
@@ -17,6 +20,8 @@ router.post(
   async (req, res) => {
     const error = validationResult(req);
     let user = await User.findOne({ email: req.body.email });
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
     // if data is invalid
     if (!error.isEmpty()) {
       res.send({ errors: error.array() });
@@ -25,9 +30,15 @@ router.post(
       res.send("Email already exists!");
       // adding data
     } else {
-      let user = new User(req.body);
+      let user = new User({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email,
+      });
+      const data = user.id
+      const authToken = jwt.sign(data,JWT_SECRET);
+      res.json({authToken});
       let result = await user.save();
-      res.send(result);
       console.log(result);
     }
   }
