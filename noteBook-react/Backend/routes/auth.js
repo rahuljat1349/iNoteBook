174d1ca5de2aba4375161645f39e5561165c46bb
Rahul Dudi <rahuldudi1349@gmail.com>
2024-01-09 17:28:45 +0530

    added validators

--- noteBook-react/Backend/routes/auth.js
+++ noteBook-react/Backend/routes/auth.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-
+const { body, validationResult } = require("express-validator");
 
 // create a user using post
-router.post("/", async (req, res) => {
-  let user = new User(req.body);
-  let result = await user.save();
-  res.send(result);
-  console.log(result);
-});
+router.post(
+  "/createuser",
+  // validator
+  [
+    body("name", "enter a valid name").isLength({ min: 3 }),
+    body("email", "enter a valid email").isEmail(),
+    body("password", "password must be at least 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const error = validationResult(req);
+    let user = await User.findOne({ email: req.body.email });
+    // if data is invalid
+    if (!error.isEmpty()) {
+      res.send({ errors: error.array() });
+      // if user already exists
+    } else if (user) {
+      res.send("Email already exists!");
+      // adding data
+    } else {
+      let user = new User(req.body);
+      let result = await user.save();
+      res.send(result);
+      console.log(result);
+    }
+  }
+);
 
 module.exports = router;
