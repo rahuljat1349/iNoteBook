4aefe4d8f14f55414d30ba92e920f7019c46e5d8
Rahul Dudi <rahuldudi1349@gmail.com>
2024-01-13 19:29:38 +0530

    working on frontend

--- noteBook-react/src/context/notes/NoteState.jsx
+++ noteBook-react/src/context/notes/NoteState.jsx
@@ -4,21 +4,9 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 export const NoteState = (props) => {
-  const s1 = {
-    name: "Rahul",
-    class: "1st",
-  };
-  const [state, setState] = useState(s1);
-  const Update = () => {
-    setTimeout(() => {
-      setState({
-        name: "Rohit",
-        class: "3rd",
-      });
-    }, 3000);
-  };
+ 
   return (
-    <NoteContext.Provider value={{ state, Update }}>
+    <NoteContext.Provider value={}>
       {props.children}
     </NoteContext.Provider>
   );
