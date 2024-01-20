556c2bdca2c8eb53f27085810034122076e9ae10
Rahul Sharma <102500734+ImRkofficial@users.noreply.github.com>
2024-01-20 12:42:06 +0530

    Error Handling While Connecting To Database

--- noteBook-react/Backend/dbConfig.js
+++ noteBook-react/Backend/dbConfig.js
@@ -3,8 +3,13 @@ const mongoose = require("mongoose");
 const url = "mongodb://localhost:27017/iNoteBook";
 
 let dbConnect = async () => {
+  try{
   await mongoose.connect(url);
   console.log("Connected to MongoDB successfully.");
+    } catch (error) {
+        console.log(`MongoDB connection FAILED: ${error.message}`);
+        process.exit(1);
+    }
 };
 
 module.exports = dbConnect;
