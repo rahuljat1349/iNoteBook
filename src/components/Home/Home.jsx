21ba54b8843c4051920679b7682889e112569d89
rahuljat1349 <rahuldudi1349@gmail.com>
2024-03-19 16:06:41 +0530

    fixed typo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,13 @@ import AlertBar from "./Alert";
 import NotesContainer from "./NotesContainer";
 
 export default function Home() {
+    useEffect(() => {
+      if (localStorage.getItem("token") || sessionStorage.getItem("token")) {
+        getNotes();
+      } else {
+        navigate("/login");
+      }
+    }, []);
   return (
     <>
       <div className="h-12 px-6 sm:px-10 lg:px-80 w-full fixed  py-3">
